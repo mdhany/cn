f8979c45163a5fa8838adf1f0bf35897b5e4a6ca
Melvin <melvin@alsu.com.do>
2014-11-06 15:49:28 -0400

    icons

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1,6 +1,6 @@
 class MobileController < ApplicationController
   before_action :authenticate_collector!
-  before_filter :have_gifts_in_stock?
+  before_action :have_gifts_in_stock?
   #before_filter :api_graph, only: [:select_friend, :publishing_post]
 
   def start
